d0862e93243c27c2574079997e88be479ef45d6b
krutiksheth <sheth.krutik@gmail.com>
2025-04-14 14:28:07 +0530

    adding navbar into the app

--- react-client/src/app/layout/App.tsx
+++ react-client/src/app/layout/App.tsx
@@ -1,27 +1,28 @@
 import {useEffect, useState} from "react";
 import {Product} from "../models/product.ts";
 import Catalog from "../../features/catalog/Catalog.tsx";
-import {Box, Container, Typography} from "@mui/material";
+import {Container} from "@mui/material";
+import NavBar from "./NavBar.tsx";
 
 function App() {
-  
+
     const [products, setProducts] = useState<Product[]>([]);
-    
+
     useEffect(() => {
         fetch("https://localhost:5001/api/products")
             .then(res => res.json())
             .then(data => setProducts(data))
             .catch(err => console.log(err));
-    },[])
-    
+    }, [])
+
     return (
-    <Container maxWidth="xl">
-        <Box sx={{ display:"flex", justifyContent:"center", gap:3, marginY:3}}>
-            <Typography variant="h4">Ecom</Typography>
-        </Box>
-        <Catalog products={products} />
-    </Container>
-  )
+        <>
+            <NavBar></NavBar>
+            <Container maxWidth="xl" sx={{mt:14}}>
+                <Catalog products={products}/>
+            </Container>
+        </>
+    )
 }
 
 export default App
---
+++ react-client/src/app/layout/NavBar.tsx
@@ -0,0 +1,13 @@
+﻿import {AppBar, Toolbar, Typography} from "@mui/material";
+
+const NavBar = () => {
+    return (
+        <AppBar position="fixed">
+            <Toolbar>
+                <Typography variant="h6">Ecom</Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default NavBar;
