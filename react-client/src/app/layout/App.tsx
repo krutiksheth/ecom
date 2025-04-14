4bd8c48a0a50ed7616eb6ca0642b0ac6a20982b2
krutiksheth <sheth.krutik@gmail.com>
2025-04-14 14:58:04 +0530

    adding dark and light mode theme

--- react-client/src/app/layout/App.tsx
+++ react-client/src/app/layout/App.tsx
@@ -1,11 +1,21 @@
 import {useEffect, useState} from "react";
 import {Product} from "../models/product.ts";
 import Catalog from "../../features/catalog/Catalog.tsx";
-import {Container} from "@mui/material";
+import {Box, Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import NavBar from "./NavBar.tsx";
 
 function App() {
-
+    const [darkMode, setDarkMode] = useState(true);
+    const palleteType = darkMode ? 'dark' : 'light';
+    const darkTheme = createTheme({
+        palette: {
+            mode: palleteType,
+            background: {
+                default:(palleteType === "dark") ? "#eaeaea" : "#121212",
+            }
+        },
+    });
+    
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
@@ -17,10 +27,16 @@ function App() {
 
     return (
         <>
-            <NavBar></NavBar>
-            <Container maxWidth="xl" sx={{mt:14}}>
-                <Catalog products={products}/>
-            </Container>
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <NavBar darkMode={darkMode} toggleDarkMode={()=> setDarkMode(!darkMode)}></NavBar>
+                <Box sx={{minHeight: '100vh', 
+                    backgroundColor: (darkMode ?  '#121212':'#eaeaea' ), py:6}}>
+                    <Container maxWidth="xl" sx={{mt:8}}>
+                        <Catalog products={products}/>
+                    </Container>
+                </Box>
+            </ThemeProvider>
         </>
     )
 }
--- react-client/src/app/layout/NavBar.tsx
+++ react-client/src/app/layout/NavBar.tsx
@@ -1,10 +1,19 @@
-﻿import {AppBar, Toolbar, Typography} from "@mui/material";
+﻿import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import {DarkMode, LightMode} from "@mui/icons-material";
 
-const NavBar = () => {
+type Props = {
+    darkMode: boolean;
+    toggleDarkMode:()=>void
+}
+
+const NavBar = ({ darkMode, toggleDarkMode}: Props) => {
     return (
         <AppBar position="fixed">
             <Toolbar>
                 <Typography variant="h6">Ecom</Typography>
+                <IconButton onClick={toggleDarkMode}>
+                    {darkMode? <DarkMode />:<LightMode sx={{color:"yellow"}}/>}
+                </IconButton>
             </Toolbar>
         </AppBar>
     );
