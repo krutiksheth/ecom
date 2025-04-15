ee125fd8a55a640e5e11ae39c9b807c5546db514
Krutik Sheth <sheth.krutik@gmail.com>
2025-04-15 18:21:33 +0530

    added httpmodule to call product api

--- angular-client/src/app/app.component.ts
+++ angular-client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./layout/header/header.component";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,21 @@ import {HeaderComponent} from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  baseUrl = 'https://localhost:5001/api';
   title = 'Ecom';
+  //old approach
+  //constructor(private http: HttpClient) { }
+  //new approach
+  private http = inject(HttpClient);
+  products: any[] =[];
+
+  ngOnInit(): void {
+    this.http.get<any[]>(this.baseUrl+'/products').subscribe({
+      next: data => this.products = data,
+      error: error => console.log(error),
+      complete:()=> console.log('complete')
+    })
+  }
 }
