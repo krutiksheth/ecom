39c8b19dacb80842b34a56e3b17d1ac994eea3db
krutiksheth <sheth.krutik@gmail.com>
2025-04-16 11:53:02 +0530

    setup react router dom

--- react-client/src/app/layout/App.tsx
+++ react-client/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
-import {useEffect, useState} from "react";
-import {Product} from "../models/product.ts";
+import {useState} from "react";
 import Catalog from "../../features/catalog/Catalog.tsx";
 import {Box, Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import NavBar from "./NavBar.tsx";
+import { Outlet } from "react-router-dom";
 
 function App() {
     const [darkMode, setDarkMode] = useState(true);
@@ -16,15 +16,6 @@ function App() {
         },
     });
     
-    const [products, setProducts] = useState<Product[]>([]);
-
-    useEffect(() => {
-        fetch("https://localhost:5001/api/products")
-            .then(res => res.json())
-            .then(data => setProducts(data))
-            .catch(err => console.log(err));
-    }, [])
-
     return (
         <>
             <ThemeProvider theme={darkTheme}>
@@ -33,7 +24,7 @@ function App() {
                 <Box sx={{minHeight: '100vh', 
                     backgroundColor: (darkMode ?  '#121212':'#eaeaea' ), py:6}}>
                     <Container maxWidth="xl" sx={{mt:8}}>
-                        <Catalog products={products}/>
+                       <Outlet />
                     </Container>
                 </Box>
             </ThemeProvider>
---
+++ react-client/src/app/routes/Routes.tsx
@@ -0,0 +1,21 @@
+﻿import {createBrowserRouter} from "react-router-dom";
+import App from "../layout/App.tsx";
+import HomePage from "../../features/home/HomePage.tsx";
+import Catalog from "../../features/catalog/Catalog.tsx";
+import ProductDetails from "../../features/catalog/ProductDetails.tsx";
+import AboutPage from "../../features/about/AboutPage.tsx";
+import ContactPage from "../../features/contact/ContactPage.tsx";
+
+export const routes = createBrowserRouter([
+    {
+        path: "/", // route route
+        element:<App />, // specify app component here
+        children: [
+            { path: "", element: <HomePage /> }, // specify home component
+            { path: "/catalog", element: <Catalog /> }, // specify catalog component
+            { path: "/catalog/:id", element: <ProductDetails /> }, // specify product details component
+            { path: "/about", element: <AboutPage /> }, // specify about component 
+            { path: "/contact", element: <ContactPage /> }, // specify contact component 
+        ]
+    }
+])
