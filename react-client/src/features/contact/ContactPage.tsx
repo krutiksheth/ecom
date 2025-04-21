a650e80f4c104f28fd40444ea84f5a76bc719b85
krutiksheth <sheth.krutik@gmail.com>
2025-04-21 12:38:46 +0530

    setup dispatch demo for contact page

--- react-client/src/features/contact/ContactPage.tsx
+++ react-client/src/features/contact/ContactPage.tsx
@@ -1,12 +1,20 @@
-﻿import {useSelector} from "react-redux";
+﻿import {useDispatch, useSelector} from "react-redux";
+import {Button, ButtonGroup, Typography} from "@mui/material";
 
 const ContactPage = () => {
 
     const data = useSelector((state:any) => state.data)
+    const dispatch = useDispatch();
     
     return (
         <div>
-             Count : {data}
+            <Typography variant="body1">
+                The data is {data}
+            </Typography>
+            <ButtonGroup variant="contained">
+              <Button color="error" onClick={()=>dispatch({ type: "DECREMENT"})}>Decrement</Button>  
+              <Button color="success" onClick={()=>dispatch({ type: "INCREMENT"})}>Increment</Button>  
+            </ButtonGroup>
         </div>
     );
 };
--- react-client/src/features/contact/counterReducer.ts
+++ react-client/src/features/contact/counterReducer.ts
@@ -6,6 +6,21 @@ const initialState: CounterState = {
     data: 42,
 }
 
-export default function counterReducer(state= initialState){
-    return state;
+export default function counterReducer(state= initialState, action : { type: string}){
+    switch(action.type){
+        
+        case 'INCREMENT':
+            return {
+                ...state, 
+                data : state.data + 1 
+            };
+        case 'DECREMENT':
+            return {
+                ...state,
+                data : state.data - 1
+            }
+        default:
+            return state
+        
+    }
 }
