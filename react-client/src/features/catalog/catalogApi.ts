7cb9f074a5f0c9135ab1fbd31012a9bdfd7343e3
Krutik Sheth <sheth.krutik@gmail.com>
2025-04-22 18:33:08 +0530

    created custom base query for rtk

--- react-client/src/features/catalog/catalogApi.ts
+++ react-client/src/features/catalog/catalogApi.ts
@@ -1,10 +1,11 @@
 ﻿//make sure to import this one
-import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {createApi} from "@reduxjs/toolkit/query/react";
 import {Product} from "../../app/models/product.ts";
+import {baseQueryWithErrorHandling} from "../../app/api/baseApi.ts";
 
 export const catalogApi= createApi({
     reducerPath: 'catalogApi',
-    baseQuery:fetchBaseQuery({ baseUrl:'https://localhost:5001/api'}),
+    baseQuery: baseQueryWithErrorHandling,
     endpoints:(builder)=>({
         fetchProducts: builder.query<Product[],void>({
             query :()=>({
