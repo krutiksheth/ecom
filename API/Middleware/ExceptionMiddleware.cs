43da4172de67a3914805793da1cb321c33dd8c60
Krutik Sheth <sheth.krutik@gmail.com>
2025-04-25 14:03:37 +0530

    Exception handler middleware (#7)
    * added a controller to test errors
    
    * added exception handling middleware

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using ILogger = Serilog.ILogger;
+
+namespace API.Middleware;
+
+public class ExceptionMiddleware(IHostEnvironment env, ILogger<ExceptionMiddleware> logger) : IMiddleware
+{
+    
+    
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(context, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext context, Exception exception)
+    {
+        logger.LogError(exception, exception.Message);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = 500;
+
+        var response = new ProblemDetails
+        {
+            Status = 500,
+            Detail = env.IsDevelopment() ? exception.StackTrace?.ToString() : null,
+            Title = exception.Message
+        };
+        
+        var options = new JsonSerializerOptions{ PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        
+        var json = JsonSerializer.Serialize(response, options);
+        
+        await context.Response.WriteAsync(json);
+    }
+}
