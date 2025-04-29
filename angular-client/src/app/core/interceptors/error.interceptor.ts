3988daf4fa10733973cf735263ca2e22328e9159
krutiksheth <sheth.krutik@gmail.com>
2025-04-29 11:56:46 +0530

    added toast(material ui snackbar) notifications

--- angular-client/src/app/core/interceptors/error.interceptor.ts
+++ angular-client/src/app/core/interceptors/error.interceptor.ts
@@ -2,16 +2,19 @@ import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
 import {Router} from "@angular/router";
 import {inject} from "@angular/core";
 import {catchError, throwError} from "rxjs";
+import {SnackbarService} from "../services/snackbar.service";
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const router = inject(Router);
+  const snackbar = inject(SnackbarService);
+
   return next(req).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 400) {
-        alert(error.error.title || error.error)
+        snackbar.error(error.error.title || error.error)
       }
       if (error.status === 401) {
-        alert(error.error.title || error.error)
+        snackbar.error(error.error.title || error.error)
       }
       if (error.status === 404) {
         router.navigateByUrl("/not-found");
