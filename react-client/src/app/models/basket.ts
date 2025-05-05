a3a01c59df97597632feee3bc58bf2aa0a9f07c0
krutiksheth <sheth.krutik@gmail.com>
2025-05-05 11:09:46 +0530

    added basketapi rtk query and used mutation instead of query for post and delete

---
+++ react-client/src/app/models/basket.ts
@@ -0,0 +1,15 @@
+﻿export type Basket ={
+    id: number
+    basketId: string
+    items: BasketItem[]
+}
+
+export type BasketItem ={
+    productId: number
+    name: string
+    price: number
+    pictureUrl: string
+    brand: string
+    type: string
+    quantity: number
+}
