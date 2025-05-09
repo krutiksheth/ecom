e06a1042312949b73c2181ce7c3aafbf622c0dce
Krutik Sheth <sheth.krutik@gmail.com>
2025-05-09 17:43:53 +0530

    refactored code to use redis instead of db

--- API/DTOs/BasketDto.cs
+++ API/DTOs/BasketDto.cs
@@ -1,4 +1,6 @@
-﻿namespace API.DTOs;
+﻿using API.Entities;
+
+namespace API.DTOs;
 
 public class BasketDto
 {
@@ -7,4 +9,53 @@ public class BasketDto
     public required string BasketId { get; set; }
 
     public List<BasketItemDto> Items { get; set; } = [];
+    
+    public void AddItem(Product product, int quantity)
+    {
+        if (product == null)
+            ArgumentNullException.ThrowIfNull(product);
+
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity must be greater than zero");
+
+        var existingItem = FindItem(product.Id);
+
+        if (existingItem != null)
+        {
+            existingItem.Quantity += quantity;
+        }
+        else
+        {
+            Items.Add(new BasketItemDto
+            {
+                ProductId = product.Id,
+                Brand = product.Brand,
+                Price = product.Price,
+                Name = product.Name,
+                PictureUrl = product.PictureUrl,
+                Type = product.Type,
+                Quantity = quantity,
+            });
+        }
+    }
+    
+    public void RemoveItem(int productId, int quantity)
+    {
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity must be greater than zero");
+
+        var existingItem = FindItem(productId);
+
+        if (existingItem == null) return;
+
+        existingItem.Quantity -= quantity;
+
+        if (existingItem.Quantity <= 0)
+            Items.Remove(existingItem);
+    }
+    
+    private BasketItemDto? FindItem(int productId)
+    {
+        return Items.FirstOrDefault(x => x.ProductId == productId);
+    }
 }
--- API/Services/BasketService.cs
+++ API/Services/BasketService.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using API.DTOs;
 using API.Entities;
 using StackExchange.Redis;
 
@@ -8,27 +9,42 @@ public class BasketService(IConnectionMultiplexer redis) : IBasketService
 {
     private readonly IDatabase _database = redis.GetDatabase();
 
-    public async Task<Basket> GetBasket(string key)
+    public async Task<BasketDto> GetBasket(string key)
     {
         var data = await _database.StringGetAsync(key);
+        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<BasketDto>(data);
+    }
+    
+    public async Task<bool> AddItemToBasket(BasketDto basket)
+    {
+        var existingBasket = await GetBasket(basket.BasketId);
 
-        return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<Basket>(data);
+        if (existingBasket != null)
+        {
+            var expiry = _database.KeyTimeToLive(basket.BasketId);
+         
+            return await _database.StringSetAsync(basket.BasketId,JsonSerializer.Serialize(basket), expiry);
+        }
+        else
+        {
+            return await _database.StringSetAsync(
+                basket.BasketId,
+                JsonSerializer.Serialize(basket),
+                TimeSpan.FromDays(30));
+        }
     }
 
-    public async Task<Basket> AddItemToBasket(Basket basket)
+    public async Task<bool> RemoveItemFromBasket(BasketDto basket)
     {
-        var created = await _database.StringSetAsync(
-            basket.BasketId,
-            JsonSerializer.Serialize(basket),
-            TimeSpan.FromDays(30));
+        var expiry = _database.KeyTimeToLive(basket.BasketId);
 
-        if (!created) return null;
-
-        return await GetBasket(basket.BasketId);
-    }
-
-    public async Task<bool> RemoveItemFromBasket(string key)
-    {
-        return await _database.KeyDeleteAsync(key);
+        if (basket.Items.Count == 0)
+        {
+           return await _database.KeyDeleteAsync(basket.BasketId);
+        }
+        else
+        {
+           return await _database.StringSetAsync(basket.BasketId,JsonSerializer.Serialize(basket), expiry);
+        }
     }
 }
