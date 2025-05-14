4ebba41d76bcbff4c6666cf58313cd96d48109b0
krutiksheth <sheth.krutik@gmail.com>
2025-05-14 13:31:05 +0530

    created endpoints for register as well as address

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required] public required string Email { get; set; } = "";
+
+    public required string Password { get; set; }
+}
--- API/Entities/User.cs
+++ API/Entities/User.cs
@@ -4,5 +4,7 @@ namespace API.Entities;
 
 public class User : IdentityUser
 {
-    
+    public int? AddressId { get; set; }
+
+    public Address? Address { get; set; }
 }
