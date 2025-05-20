4d7fff61099be8383d77d1f94fbd628cccd759ba
krutiksheth <sheth.krutik@gmail.com>
2025-05-20 13:09:27 +0530

    created payment service and added two properties in basket to store stripe information

---
+++ API/Services/PaymentService.cs
@@ -0,0 +1,39 @@
+﻿using API.Entities;
+using Stripe;
+
+namespace API.Services;
+
+public class PaymentService(IConfiguration config)
+{
+    public async Task<PaymentIntent> CreateOrUpdatePaymentIntent(Basket basket)
+    {
+        StripeConfiguration.ApiKey = config["StripeSettings:SecretKey"];
+        var service = new PaymentIntentService();
+        var intent = new PaymentIntent();
+        var subTotal = basket.Items.Sum(x => x.Quantity * x.Product.Price);
+        var delivery = subTotal > 10000 ? 0 : 500;
+
+        if (string.IsNullOrEmpty(basket.PaymentIntentId))
+        {
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = subTotal + delivery,
+                Currency = "usd",
+                PaymentMethodTypes = ["cards"]
+            };
+
+            intent = await service.CreateAsync(options);
+        }
+        else
+        {
+            var options = new PaymentIntentUpdateOptions
+            {
+                Amount = subTotal + delivery
+            };
+
+            await service.UpdateAsync(basket.PaymentIntentId, options);
+        }
+
+        return intent;
+    }
+}
