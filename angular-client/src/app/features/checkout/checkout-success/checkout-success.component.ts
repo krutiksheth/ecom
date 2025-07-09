9815b47d07d341c7e42c0988741d25070fafdf86
krutiksheth <sheth.krutik@gmail.com>
2025-07-09 15:07:26 +0530

    design checkout success page

--- angular-client/src/app/features/checkout/checkout-success/checkout-success.component.ts
+++ angular-client/src/app/features/checkout/checkout-success/checkout-success.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-checkout-success',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatButton
+  ],
   templateUrl: './checkout-success.component.html',
   styleUrl: './checkout-success.component.scss'
 })
