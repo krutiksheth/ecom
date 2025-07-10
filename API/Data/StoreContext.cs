8b6d27753308cb24325e568ef4098c1c6121a3a2
krutiksheth <sheth.krutik@gmail.com>
2025-07-10 14:30:56 +0530

    created order entity

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Immutable;
-using API.Entities;
+﻿using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -12,13 +12,15 @@ public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(op
 
     public DbSet<Basket> Baskets { get; set; }
 
+    public required DbSet<Order> Orders { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<IdentityRole>().HasData(
-            new IdentityRole { Id="8b9f8636-c480-40ad-bdfa-a844109b3a69", Name = "Admin", NormalizedName = "ADMIN" },
-            new IdentityRole { Id="f36f9c48-95f7-4f0a-bc8a-60ad58abe512",Name = "Member", NormalizedName = "MEMBER" }
+            new IdentityRole { Id = "8b9f8636-c480-40ad-bdfa-a844109b3a69", Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole { Id = "f36f9c48-95f7-4f0a-bc8a-60ad58abe512", Name = "Member", NormalizedName = "MEMBER" }
         );
     }
 }
